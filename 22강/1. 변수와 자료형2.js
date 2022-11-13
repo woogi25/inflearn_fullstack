dda039946e390386bab68f5bce459236e5ca5689
woogi25 <hyunwook1891@gmail.com>
2022-11-13 13:30:38 +0900

    JS 변수, 자료형 공부

---
+++ 22강/1. 변수와 자료형2.js
@@ -0,0 +1,22 @@
+//숫자형 자료형 선언
+//var 재선언O 재할당O, (let 재할당O, const재할당X) = 재선언X
+var num = 1;
+
+var num = 10;
+
+console.log(num)
+
+let num2 = 2;
+num2 = 20;
+
+const num3 = 3;
+// num3 = 30;
+
+//문자형 자료형 선언
+let string = "안녕"
+
+//논리형 자료형 선언
+let bool = true;
+let t = 100 > 10;
+
+console.log(t);
